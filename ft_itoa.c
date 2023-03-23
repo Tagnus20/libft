c82bb6ba1b8d3bfd54717fc96d26a504b3d8505d
Alvaro <alvaro_verdejo123@hotmail.com>
2023-03-24 00:46:19 +0100

    24 00:00

--- ft_itoa.c
+++ ft_itoa.c
@@ -5,12 +5,47 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: averdejo <averdejo@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/22 18:27:17 by averdejo          #+#    #+#             */
-/*   Updated: 2023/03/22 18:29:23 by averdejo         ###   ########.fr       */
+/*   Created: 2023/03/23 13:25:51 by averdejo          #+#    #+#             */
+/*   Updated: 2023/03/24 00:37:48 by averdejo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+#include "libft.h"
+
 char	*ft_itoa(int n)
 {
-	
+	char	*x;
+	int		n2;
+	int		len;
+
+	n2 = n;
+	len = 0;
+	while (n2 != 0)
+	{
+		len++;
+		n2 = n2 / 10;
+	}
+	x = (char *) malloc((len + 1) * sizeof(char));
+	if (!x)
+		return (NULL);
+	x[len] = '\0';
+	while (n != 0 && len != 0)
+	{
+		n2 = n % 10;
+		x[len - 1] = n2 + '0';
+		len--;
+		n = n / 10;
+	}
+	printf("%s\n", x);
+	return (x);
 }
+
+/*int	main(void)
+{
+	int	n = 150;
+
+	printf("%s\n", ft_itoa(n));
+	printf("%d\n", n);
+	return (0);
+}*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: averdejo <averdejo@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 11:23:47 by averdejo          #+#    #+#             */
-/*   Updated: 2023/03/21 14:27:10 by averdejo         ###   ########.fr       */
+/*   Updated: 2023/03/24 00:34:03 by averdejo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,5 +42,6 @@ char	*ft_strdup(const char *s1);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_itoa(int n);
 
 #endif
